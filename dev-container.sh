c18b65e2353b8ff50616cacf7d69b6c00d7ad2b8
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-12-03 14:50:04 +0700

    Add unzip as the ubuntu user.

--- dev-container.sh
+++ dev-container.sh
@@ -11,7 +11,7 @@ fi
 lxc ls | grep "\<$1\>" 1>/dev/null
 grep_status=$?
 if [ $grep_status -eq 0 ]; then
-  echo "A container $1 already exists. Removing it first."
+  echo "Container $1 already exists. Removing it first."
   lxc rm --force "$1"
 fi
 
@@ -24,15 +24,17 @@ sleep 4
 lxc exec "$1" -- wget -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.appimage
 lxc exec "$1" -- chmod 755 /usr/local/bin/nvim
 
-lxc exec "$1" -- mkdir --parents /home/ubuntu/.config/nvim
 sleep 4
+lxc exec "$1" -- mkdir --parents /home/ubuntu/.config/nvim
 lxc exec "$1" -- git clone https://github.com/ChrisTaylorDeveloper/LazyVim.git /home/ubuntu/.config/nvim
+
 lxc exec "$1" -- chown -R ubuntu:ubuntu /home/ubuntu/
+
+lxc exec "$1" --user 1000 -- sudo apt-get install unzip
 
 # Need to add these:
 # sudo apt-get install unzip
 # set -o vi
 # set correct git author name and email in /home/ubuntu/.gitconfig
-# install neovim
 # set git editor to nvim
 # git config --global core.editor "vim"
