4b6d95cd6298b35f7fad16c475f707a8f34e4151
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-12-03 14:40:06 +0700

    Add nvim appimage.

--- dev-container.sh
+++ dev-container.sh
@@ -20,6 +20,13 @@ lxc launch ubuntu:24.04 "$1"
 # Do not start the container just because the host has started!
 lxc config set "$1" boot.autostart false
 
+sleep 4
+lxc exec "$1" -- wget -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.appimage
+lxc exec "$1" -- chmod 755 /usr/local/bin/nvim
+
+# chmod u+x nvim-linux-x86_64.appimage
+# ./nvim-linux-x86_64.appimage
+
 # Need to add these:
 # sudo apt-get install unzip
 # set -o vi
@@ -29,7 +36,6 @@ lxc config set "$1" boot.autostart false
 # git config --global core.editor "vim"
 
 lxc exec "$1" -- mkdir --parents /home/ubuntu/.config/nvim
-echo Sleeping ...
 sleep 4
 lxc exec "$1" -- git clone https://github.com/ChrisTaylorDeveloper/LazyVim.git /home/ubuntu/.config/nvim
 lxc exec "$1" -- chown -R ubuntu:ubuntu /home/ubuntu/
